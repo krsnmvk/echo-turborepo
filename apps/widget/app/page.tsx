3ef8eadee2cce29d80b2f6c16af65c4597470ced
krsnmvk <krsnmvk@gmail.com>
2025-08-09 19:10:54 +0700

    05: ai voice assistant

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,48 +1,40 @@
 'use client';
 
-import {
-  Authenticated,
-  Unauthenticated,
-  AuthLoading,
-  useMutation,
-  useQuery,
-} from 'convex/react';
 import { Button } from '@workspace/ui/components/button';
-import { api } from '@workspace/backend/convex/_generated/api';
-import {} from 'convex/react';
-import { Loader2Icon } from 'lucide-react';
+import { useVapi } from '@/modules/widget/hooks/use-vapi';
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
-  const create = useMutation(api.users.create);
+  const {
+    endtCall,
+    isConnected,
+    isConnecting,
+    isSpeaking,
+    startCall,
+    transcript,
+  } = useVapi();
 
   return (
-    <>
-      <AuthLoading>
-        <div className="flex items-center justify-center min-h-svh">
-          <div className="flex flex-col items-center justify-center gap-4">
-            <Loader2Icon className="size-10 animate-spin" />
-          </div>
-        </div>
-      </AuthLoading>
-      <Authenticated>
-        <div className="flex items-center justify-center min-h-svh">
-          <div className="flex flex-col items-center justify-center gap-4">
-            <h1 className="text-2xl font-bold">Hello World/widget</h1>
-            <Button type="button" onClick={() => create()} size="sm">
-              Create Users
-            </Button>
-            {JSON.stringify(users, null, 2)}
-          </div>
-        </div>
-      </Authenticated>
-      <Unauthenticated>
-        <div className="flex items-center justify-center min-h-svh">
-          <div className="flex flex-col items-center justify-center gap-4">
-            <h1 className="text-2xl font-bold">Must be login/widget</h1>
-          </div>
-        </div>
-      </Unauthenticated>
-    </>
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Hello World/widget</h1>
+        <Button type="button" onClick={() => startCall()} size="sm">
+          Start Call
+        </Button>
+        <Button
+          type="button"
+          onClick={() => endtCall()}
+          variant="destructive"
+          size="sm"
+        >
+          End Call
+        </Button>
+
+        <span>isConnected {`${isConnected}`}</span>
+        <span>isConnecting {`${isConnecting}`}</span>
+        <span>isSpeaking {`${isSpeaking}`}</span>
+
+        {JSON.stringify(transcript, null, 2)}
+      </div>
+    </div>
   );
 }
