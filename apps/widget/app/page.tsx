8a6b360fb363c92c15e984295adf7a700a5e0403
krsnmvk <krsnmvk@gmail.com>
2025-08-10 08:26:23 +0700

    08: widget layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,40 +1,12 @@
-'use client';
+import { use } from 'react';
+import WidgetView from '@/modules/widget/ui/views/widget-view';
 
-import { Button } from '@workspace/ui/components/button';
-import { useVapi } from '@/modules/widget/hooks/use-vapi';
+type Props = {
+  searchParams: Promise<{ organizationId: string }>;
+};
 
-export default function Page() {
-  const {
-    endtCall,
-    isConnected,
-    isConnecting,
-    isSpeaking,
-    startCall,
-    transcript,
-  } = useVapi();
+export default function Home({ searchParams }: Props) {
+  const { organizationId } = use(searchParams);
 
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/widget</h1>
-        <Button type="button" onClick={() => startCall()} size="sm">
-          Start Call
-        </Button>
-        <Button
-          type="button"
-          onClick={() => endtCall()}
-          variant="destructive"
-          size="sm"
-        >
-          End Call
-        </Button>
-
-        <span>isConnected {`${isConnected}`}</span>
-        <span>isConnecting {`${isConnecting}`}</span>
-        <span>isSpeaking {`${isSpeaking}`}</span>
-
-        {JSON.stringify(transcript, null, 2)}
-      </div>
-    </div>
-  );
+  return <WidgetView organizationId={organizationId} />;
 }
